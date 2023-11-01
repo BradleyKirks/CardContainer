6a26b9ed2e00bbd7edaf0f280509d83d912a6af9
Bradley Kirks <spinnyorb23@aim.com>
2023-11-01 16:01:33 -0400

    card container website

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+
+export default function Home(){
+    return(
+        <>
+        <h2>Home Component</h2>
+        <Nav/>
+        <Footer />
+        </>
+    )
+}
